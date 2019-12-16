e263ef0de5144a5249e6a1f8ecd512b07ed9e632
Balaji Sivaraman <balaji@balajisivaraman.com>
2019-12-16 10:13:17 +0530

    refactor: provide cloned inputs to each part method

--- src/day01.rs
+++ src/day01.rs
@@ -5,18 +5,18 @@ use std::result::Result;
 
 use utils::read_lines_as_int_vector;
 
-fn fuel_required_for_mass(mass: &i32) -> i32 {
+fn fuel_required_for_mass(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
-fn fuel_for_modules(module_masses: &Vec<i32>) -> i32 {
+fn fuel_for_modules(module_masses: Vec<i32>) -> i32 {
     module_masses
-        .iter()
+        .into_iter()
         .map(fuel_required_for_mass)
         .fold(0, |acc, f| acc + f)
 }
 
-fn additional_fuel_for_modules(module_masses: &Vec<i32>) -> i32 {
+fn additional_fuel_for_modules(module_masses: Vec<i32>) -> i32 {
     let additional_fuel_calculator = |mass| {
         let mut fuel_required = fuel_required_for_mass(mass);
         let mut total_fuel_required = 0;
@@ -25,20 +25,23 @@ fn additional_fuel_for_modules(module_masses: &Vec<i32>) -> i32 {
                 break;
             }
             total_fuel_required += fuel_required;
-            fuel_required = fuel_required_for_mass(&fuel_required);
+            fuel_required = fuel_required_for_mass(fuel_required);
         }
         total_fuel_required
     };
     module_masses
-        .iter()
+        .into_iter()
         .map(additional_fuel_calculator)
         .fold(0, |acc, f| acc + f)
 }
 
 fn main() -> Result<(), Error> {
     let input = read_lines_as_int_vector("input/day01")?;
-    println!("Day 01, Part 1: {}", fuel_for_modules(&input));
-    println!("Day 01, Part 2: {}", additional_fuel_for_modules(&input));
+    println!("Day 01, Part 1: {}", fuel_for_modules(input.clone()));
+    println!(
+        "Day 01, Part 2: {}",
+        additional_fuel_for_modules(input.clone())
+    );
     Ok(())
 }
 
